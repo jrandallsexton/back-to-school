44709c0c45a612326ecc68617a050de72d4de36d
Randall Sexton <jrandallsexton@gmail.com>
2024-04-27 13:21:06 -0400

    tried to do better than Alt, but did not work out.  aborting more testing.  done. finito.

--- src/CSharp/BackToSchool.CSharp/Misc/BinaryPatternMatchingBenchmarks.cs
+++ src/CSharp/BackToSchool.CSharp/Misc/BinaryPatternMatchingBenchmarks.cs
@@ -41,15 +41,6 @@ namespace BackToSchool.CSharp.Misc
         }
 
         [Benchmark]
-        public void ReviewedAlt2()
-        {
-            for (var i = 0; i < RepetitionCount; i++)
-            {
-                BinaryPatternMatching.MatchReviewAlt2(Pattern, Source);
-            }
-        }
-
-        [Benchmark]
         public void Optimized()
         {
             for (var i = 0; i < RepetitionCount; i++)
