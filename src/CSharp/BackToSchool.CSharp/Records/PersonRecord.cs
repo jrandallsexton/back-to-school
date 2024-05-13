c3903a1a24fa3e3c3db4f70dd13b0d7a091097de
Randall Sexton <jrandallsexton@gmail.com>
2024-05-13 19:51:12 -0400

    working on some record stuff

---
+++ src/CSharp/BackToSchool.CSharp/Records/PersonRecord.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace BackToSchool.CSharp.Records
+{
+    /// <summary>
+    /// https://www.youtube.com/watch?v=VouNkrgkH78
+    /// More goodness from Zoran
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <param name="BirthDate"></param>
+    public record PersonRecord(string Name, DateOnly BirthDate);
+}
---
+++ test/CSharp/BackToSchool.CSharp.Tests/Records/PersonTests.cs
@@ -0,0 +1,33 @@
+﻿
+using BackToSchool.CSharp.Records;
+
+using FluentAssertions;
+
+using System;
+using System.Linq;
+
+using Xunit;
+
+namespace BackToSchool.CSharp.Tests.Records
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void ShowsDistinct()
+        {
+            // arrange
+            var people = new PersonRecord[]
+            {
+                new("Joe", new DateOnly(1997, 4, 28)),
+                new("Jane", new DateOnly(1982, 5, 14)),
+                new("Joe", new DateOnly(1986, 7, 19)),
+                new("Joe", new DateOnly(1997, 4, 28)),
+                new("Jane", new DateOnly(1982, 5, 14))
+            };
+
+            // act & assert
+            people.Length.Should().Be(5);
+            people.Distinct().Count().Should().Be(3);
+        }
+    }
+}
