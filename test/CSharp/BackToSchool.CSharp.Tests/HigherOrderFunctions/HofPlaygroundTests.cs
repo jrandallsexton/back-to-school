f80df870d93d701c0ed0bd8665ad5dae5de10414
Randall Sexton <jrandallsexton@gmail.com>
2024-05-16 18:10:08 -0400

    HOF work

--- test/CSharp/BackToSchool.CSharp.Tests/HigherOrderFunctions/HofPlaygroundTests.cs
+++ test/CSharp/BackToSchool.CSharp.Tests/HigherOrderFunctions/HofPlaygroundTests.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿
+using BackToSchool.CSharp.HigherOrderFunctions;
+
+using FluentAssertions;
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using Xunit;
 
 namespace BackToSchool.CSharp.Tests.HigherOrderFunctions
 {
     public class HofPlaygroundTests
     {
+        [Fact]
+        public void TransformTest()
+        {
+            // arrange
+            var oldList = new List<int> { 100, 200, 300, 400, 500 };
+
+            static int DivideBy2(int num) => num / 2;
+
+            // act
+            //var result = new HofPlayground().Transform(oldList, i => i / 2);
+            var result = new HofPlayground().Transform(oldList, DivideBy2);
+
+            // assert
+            result[0].Should().Be(oldList[0] / 2);
+        }
     }
 }
