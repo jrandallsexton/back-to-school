8968a8acaf0d03f3c7927cd406306232ee7a2ecb
Randall Sexton <jrandallsexton@gmail.com>
2024-05-16 17:32:58 -0400

    HOF scaffolding

---
+++ src/CSharp/BackToSchool.CSharp/HigherOrderFunctions/HofPlayground.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace BackToSchool.CSharp.HigherOrderFunctions
+{
+    public class HofPlayground
+    {
+        public List<TOutput> Transform<TInput, TOutput>(List<TInput> list, Func<TInput, TOutput> transformer)
+        {
+            var newList = new List<TOutput>();
+
+            foreach (var input in list)
+            {
+                var newItem = transformer(input);
+                newList.Add(newItem);
+            }
+
+            return newList;
+        }
+    }
+}
