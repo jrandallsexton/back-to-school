91a63914f09f08ec8a3252b97e961f80355fb07c
Randall Sexton <jrandallsexton@gmail.com>
2024-05-31 12:49:34 -0400

    bsort

--- src/CSharp/BackToSchool.CSharp/Sorting/BubbleSort.cs
+++ src/CSharp/BackToSchool.CSharp/Sorting/BubbleSort.cs
@@ -8,5 +8,13 @@ namespace BackToSchool.CSharp.Sorting
 {
     public class BubbleSort
     {
+        public void Sort(int[] numbers)
+        {
+            if (numbers.Length == 0 || numbers.Length == 1)
+                return;
+
+            var left = numbers[0];
+            var right = numbers[1];
+        }
     }
 }
