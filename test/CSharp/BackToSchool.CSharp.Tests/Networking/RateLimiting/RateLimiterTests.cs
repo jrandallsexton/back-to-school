978bf33b65cf086c23203c3df6698236d452baf9
Randall Sexton <jrandallsexton@gmail.com>
2025-02-25 17:53:40 -0500

    reminder to add unit tests for rate limiter

---
+++ test/CSharp/BackToSchool.CSharp.Tests/Networking/RateLimiting/RateLimiterTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BackToSchool.CSharp.Tests.Networking.RateLimiting
+{
+    public class RateLimiterTests
+    {
+        [Fact]
+        public void WhenFoo_DoesBar()
+        {
+            // TODO: migrate these from the other project
+        }
+    }
+}
